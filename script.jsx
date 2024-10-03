ac376a4829d03d643a6ea36f1da459e2911dfcd3
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-10-02 23:19:51 -0400

    finished markdown

--- script.jsx
+++ script.jsx
@@ -1,20 +1,38 @@
-marked.setOPtions({
- breaks: true,
-});
-const renderer = new marked.Renderer();
-function App(){
-    const [input, getInput] =useState('');
-    const getMarkdownText = () => {
-        const rawMarkup = marked(input, { sanitize: true });
-        return { __html: rawMarkup };
-    };
-    return(
-        <div id="text">
-            <textarea onChange={(e) => getInput(e.target.value)}/>
-    <div id="textarea"   dangerouslySetInnerHTML={{__html: marked(markdown,{renderer: renderer}}}/>
-      </div>
+ const {useState} = React;
+function Edit() {
+    const [input, getInput] = useState(`# Welcome to my React Markdown Previewer! (H1)
+
+## This is a sub-heading... (H2)
+
+Here’s a [link to freeCodeCamp](https://www.freecodecamp.org).
+
+Here's some inline code: \`<div></div>\` between two backticks.
+
+\`\`\`
+// This is a code block
+function helloWorld() {
+    console.log("Hello, world!");
+}
+\`\`\`
+
+- This is a list item.
+
+> This is a blockquote.
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+You can also make text **bold**... whoa!`);
+    
+    return (
+        <div id="container">
+            <textarea 
+                id="editor" 
+                onChange={(e) => getInput(e.target.value)} 
+                value={input}
+            />
+            <div id="preview" dangerouslySetInnerHTML={{ __html: marked(input) }}></div>
+        </div>
     );
 }
 
-
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.render(<Edit/>,document.getElementById("root"));
